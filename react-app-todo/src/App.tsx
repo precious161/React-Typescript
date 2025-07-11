23b6e4024bc133d1c9ceb5d5f2c69755bbf47b1b
feyruza dawud <feyruza.dawud-ug@aau.edu.et>
2025-07-11 03:42:25 +0300

    Fetched todos data for the todo app from an API using useState and useEffect React hooks

--- react-app-todo/src/App.tsx
+++ react-app-todo/src/App.tsx
@@ -1,6 +1,13 @@
+import { useState, useEffect } from "react";
 import "./App.css";
-
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((res) => console.log(res));
+  }, []);
   return (
     <div className="App">
       <h1>Hello World</h1>
